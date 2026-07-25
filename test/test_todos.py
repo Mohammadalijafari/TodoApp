f1303c1c64003198529fd57664b9c7fde0b1d1ee
MohammadaliJafari <m.alijafari96@gmail.com>
2026-07-25 11:48:16 +0330

    Modified routers/todos.py and test/test_todos.py

--- test/test_todos.py
+++ test/test_todos.py
@@ -61,7 +61,7 @@ def test_todo():
 
 
 def test_read_all_authenticated(test_todo):
-    response = client.get("/")
+    response = client.get("/todos")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [
         {
@@ -76,7 +76,7 @@ def test_read_all_authenticated(test_todo):
 
 
 def test_read_one_authenticated(test_todo):
-    response = client.get("/todo/1")
+    response = client.get("/todos/todo/1")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         "id": 1,
@@ -89,7 +89,7 @@ def test_read_one_authenticated(test_todo):
 
 
 def test_read_one_authenticated_not_found(test_todo):
-    response = client.get("/todo/99")
+    response = client.get("/todos/todo/99")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "Todo not found"}
 
@@ -102,7 +102,7 @@ def test_create_todo(test_todo):
         "complete": False,
     }
 
-    response = client.post("/todo/", json=request_data)
+    response = client.post("/todos/todo/", json=request_data)
     assert response.status_code == status.HTTP_201_CREATED
 
     db = TestingSessionLocal()
@@ -121,7 +121,7 @@ def tst_create_todo(test_todo):
         "complete": False,
     }
 
-    response = client.put("/todo/1", json=request_data)
+    response = client.put("/todos/todo/1", json=request_data)
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert response.json() == {
         "title": "changed title",
@@ -132,7 +132,7 @@ def tst_create_todo(test_todo):
 
 
 def test_delete_todo(test_todo):
-    response = client.delete("/todo/1")
+    response = client.delete("/todos/todo/1")
     assert response.status_code == status.HTTP_204_NO_CONTENT
 
     db = TestingSessionLocal()
@@ -141,6 +141,6 @@ def test_delete_todo(test_todo):
 
 
 def test_delete_todo_not_found():
-    response = client.delete("/todo/999")
+    response = client.delete("/todos/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "Todo not found"}
